39b2151745faa3468d5393f4f7ea0c555d58e710
Shawn Witemyre <switemyre@comcast.net>
2024-11-09 22:56:18 -0500

    setup

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>
+    <main>
+      <header>
+        <h1>Todo App</h1>
+        <img src="./assets/pinia-logo.svg" alt="pinia logo" />
+      </header>
+    </main>
+  </h1>
+</template>
+<script setup></script>
+
+<style scoped>
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+header {
+  display: flex;
+}
+</style>
