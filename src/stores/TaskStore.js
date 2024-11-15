6d2931673216f7f199a689833f7548675063d787
Shawn Witemyre <switemyre@comcast.net>
2024-11-14 21:07:09 -0500

    added getters to pinia store

--- src/stores/TaskStore.js
+++ src/stores/TaskStore.js
@@ -3,8 +3,21 @@ import { defineStore } from "pinia";
 export const useTaskStore = defineStore("taskStore", {
   state: () => ({
     tasks: [
-      { id: 1, title: "play magic the gathering", isFav: false },
-      { id: 2, title: "play warhammer 40k", isFav: false },
+      { id: 1, title: "buy some milk", isFavorite: false },
+      { id: 2, title: "play Gloomhaven", isFavorite: true },
     ],
   }),
+  getters: {
+    favorite() {
+      return this.tasks.filter((t) => t.isFavorite);
+    },
+    favoriteCount() {
+      return this.tasks.reduce((p, c) => {
+        return c.isFavorite ? p + 1 : p;
+      }, 0);
+    },
+    totalCount: (state) => {
+      return state.tasks.length;
+    },
+  },
 });
