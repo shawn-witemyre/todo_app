2149bb322b3020fe1a0e78d6264545af195bdf67
Shawn Witemyre <switemyre@comcast.net>
2024-11-14 21:07:41 -0500

    created toggle for favorites and all tasks

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,37 @@
 <template>
   <main>
     <header>
-      <h1>Todo App</h1>
-      <img src="./assets/pinia-logo.svg" alt="pinia logo" />
+      <img src="https://pinia.vuejs.org/logo.svg" alt="pinia logo" />
+      <h1>Pinia Tasks</h1>
     </header>
-  </main>
 
-  <div class="task-list">
-    <div v-for="task in taskStore.tasks" :key="task.id">
-      <TaskDetail :task="task" />
+    <nav class="filter">
+      <button @click="filter = 'all'">All tasks</button>
+      <button @click="filter = 'favorite'">Favorite tasks</button>
+    </nav>
+
+    <div class="task-list" v-if="filter === 'all'">
+      <p>You have {{ taskStore.totalCount }} tasks left to do</p>
+      <div v-for="task in taskStore.tasks" :key="task.id">
+        <TaskDetails :task="task" />
+      </div>
     </div>
-  </div>
+    <div class="task-list" v-if="filter === 'favorite'">
+      <p>You have {{ taskStore.favoriteCount }} tasks in your favorite list</p>
+      <div v-for="task in taskStore.favorite" :key="task.id">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+  </main>
 </template>
 <script setup>
-import TaskDetail from "./components/TaskDetail.vue";
+import TaskDetails from "./components/TaskDetails.vue";
 import { useTaskStore } from "./stores/TaskStore";
+import { ref } from "vue";
 
 const taskStore = useTaskStore();
+
+const filter = ref("all");
 </script>
 
 <style scoped>
