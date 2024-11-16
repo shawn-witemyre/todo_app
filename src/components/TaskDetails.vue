91a200761ccc8b3e6e261936f8681bc61742a125
Shawn Witemyre <switemyre@comcast.net>
2024-11-15 23:26:18 -0500

    added delete and toggle tasks functionality to taskdetails

--- src/components/TaskDetails.vue
+++ src/components/TaskDetails.vue
@@ -2,14 +2,24 @@
   <div class="task">
     <h3>{{ task.title }}</h3>
     <div class="icons">
-      <i class="material-icons">delete</i>
-      <i class="material-icons">favorite</i>
+      <p>{{ task.id }}</p>
+      <i class="material-icons" @click="taskStore.deleteTask(task.id)"
+        >delete
+      </i>
+
+      <i class="material-icons" @click="taskStore.toggleFavorite(task.id)"
+        >favorite</i
+      >
     </div>
   </div>
 </template>
 
 <script setup>
+import { useTaskStore } from "../stores/TaskStore";
+
 const props = defineProps(["task"]);
+
+const taskStore = useTaskStore();
 </script>
 
 <style scoped>
