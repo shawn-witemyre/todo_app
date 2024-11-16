fe3d780aab4b6fb2b33234cb7b32526b8b56b022
Shawn Witemyre <switemyre@comcast.net>
2024-11-15 23:25:12 -0500

    created TaskForm component

---
+++ src/components/TaskForm.vue
@@ -0,0 +1,27 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input type="text" placeholder="Enter new task" v-model="newTask" />
+    <button>Add Task</button>
+  </form>
+</template>
+
+<script setup>
+import { useTaskStore } from "../stores/TaskStore";
+import { ref } from "vue";
+
+const taskStore = useTaskStore();
+
+const newTask = ref("");
+
+function handleSubmit() {
+  if (newTask.value.length > 0) {
+    taskStore.addTask({
+      title: newTask.value,
+      isFavorite: false,
+      id: Math.floor(Math.random() * 10000),
+    });
+
+    newTask.value = "";
+  }
+}
+</script>
