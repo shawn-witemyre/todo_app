4462a4790cfc68000fcadc5c93b570f9b24a1226
Shawn Witemyre <switemyre@comcast.net>
2024-11-25 17:31:33 -0500

    added storeToRef to make it not as verbose

--- src/App.vue
+++ src/App.vue
@@ -14,19 +14,23 @@
       <button @click="filter = 'favorite'">Favorite tasks</button>
     </nav>
 
-    <div class="loading" v-if="taskStore.isLoading">Loading tasks...</div>
+    <div class="loading" v-if="isLoading">Loading tasks...</div>
 
     <div class="task-list" v-if="filter === 'all'">
-      <p>You have {{ taskStore.totalCount }} tasks left to do</p>
-      <div v-for="task in taskStore.tasks" :key="task.id">
+      <p>You have {{ totalCount }} tasks left to do</p>
+      <div v-for="task in tasks" :key="task.id">
         <TaskDetails :task="task" />
       </div>
     </div>
     <div class="task-list" v-if="filter === 'favorite'">
-      <p>You have {{ taskStore.favoriteCount }} tasks in your favorite list</p>
-      <div v-for="task in taskStore.favorite" :key="task.id">
+      <p>You have {{ favoriteCount }} tasks in your favorite list</p>
+      <div v-for="task in favorite" :key="task.id">
         <TaskDetails :task="task" />
       </div>
+    </div>
+
+    <div class="reset-button">
+      <button @click="taskStore.$reset">Reset</button>
     </div>
   </main>
 </template>
@@ -35,8 +39,12 @@ import TaskForm from "./components/TaskForm.vue";
 import TaskDetails from "./components/TaskDetails.vue";
 import { useTaskStore } from "./stores/TaskStore";
 import { ref } from "vue";
+import { storeToRefs } from "pinia";
 
 const taskStore = useTaskStore();
+
+const { tasks, isLoading, favorite, totalCount, favoriteCount } =
+  storeToRefs(taskStore);
 
 taskStore.getTasks();
 
@@ -81,5 +89,10 @@ nav button {
 .task-list {
   max-width: 640px;
   margin: 20px auto;
+}
+
+.reset-button {
+  display: flex;
+  justify-content: center;
 }
 </style>
